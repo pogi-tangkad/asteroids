12cf04c3afa5e5c52818541d36ac74141bd8f88b
pogi-tangkad <pogi.tangkad@gmail.com>
2025-02-06 22:25:22 -0600

    add circleshape.py and player.py and update main.py and constants.py: now draws player that can rotate and move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -10,6 +11,14 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
     while(True):
         screen.fill("Black")
 
@@ -17,6 +26,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.draw(screen)
+        
+        player.update(dt)
+        
+        dt = clock.tick(60) / 1000
+        
 
         pygame.display.flip()
 
